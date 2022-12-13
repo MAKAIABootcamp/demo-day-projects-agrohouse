9687c8c1257cea80d474ccadf746000322ba3b08
Daniela Aguirre <daniaguirreorozco@gmail.com>
2022-12-12 21:45:30 -0500

    events edited

--- src/components/MyEvents/MyEventsCards.jsx
+++ src/components/MyEvents/MyEventsCards.jsx
@@ -1,3 +1,4 @@
+import { TopicOutlined } from "@mui/icons-material";
 import { Card, CardActionArea, CardContent, Typography } from "@mui/material";
 import { Box } from "@mui/system";
 import React, { useEffect } from "react";
@@ -22,19 +23,19 @@ const MyEventsCards = () => {
           ? (
             events.map((event, index) => (
               <Box key={index} sx={{ width: 275, height: 'fit-content' }}>
-                <Card variant="outlined" sx={{ backgroundColor: '#8BC349' }}>
+                <Card variant="outlined" sx={{ backgroundColor: '#ffff' }}>
                   <CardActionArea>
                     <CardContent>
-                      <Typography variant="h5" component="div">
+                      <Typography variant="h5" component="div" sx={{ color: '#00960F' }} >
                         {event.name}
                       </Typography>
-                      <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                      <Typography sx={{ mb: 1.5, color: '#F5B916'}} color="text.secondary" >
                         Productos del {event.region}
                       </Typography>
                       <Typography variant="body2">
                         {event.ubication}
                       </Typography>
-                      <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                      <Typography sx={{ mb: 1.5, color:'#220BD4', mt:1}} color="text.secondary">
                         {event.date}
                       </Typography>
                     </CardContent>
